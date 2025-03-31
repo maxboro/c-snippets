c997b1df0b23c1d14ca8f87d652a4cff879247a6
Max B. <maksym_borovyk@ukr.net>
2025-03-31 10:31:25 +0300

    Simplify append function, add length

--- src/dyn_array.c
+++ src/dyn_array.c
@@ -13,27 +13,18 @@ void init_array(struct DynArrayInt* arr_ptr){
 }
 
 void append(struct DynArrayInt* arr_ptr, int element){
-    if (arr_ptr->head_ptr == NULL){
-        arr_ptr->head_ptr = (int*)malloc(sizeof(int));
-        if (arr_ptr->head_ptr == NULL){
-            fprintf(stderr, "Memory weren't allocated successfully\n");
-        }
-        *(arr_ptr->head_ptr) = element;
-        arr_ptr->n_elements = 1;
-        printf("Element added at %p\n", arr_ptr->head_ptr);
-    } else {
-        int new_n_elements = arr_ptr->n_elements + 1;
-        int* new_head_ptr = (int*)realloc(arr_ptr->head_ptr, new_n_elements * sizeof(int));
-        if (new_head_ptr == NULL){
-            fprintf(stderr, "Memory weren't reallocated successfully\n");
-        }
-        *(new_head_ptr + new_n_elements - 1) = element;
-        printf("Element added at %p\n", new_head_ptr + new_n_elements - 1);
-        
-        
-        arr_ptr->head_ptr = new_head_ptr;
-        arr_ptr->n_elements = new_n_elements;
+    int new_n_elements = arr_ptr->n_elements + 1;
+    int* new_head_ptr = (int*)realloc(arr_ptr->head_ptr, new_n_elements * sizeof(int));
+    if (new_head_ptr == NULL){
+        fprintf(stderr, "Memory weren't (re)allocated successfully\n");
     }
+    *(new_head_ptr + new_n_elements - 1) = element;
+    printf("Element added at %p\n", new_head_ptr + new_n_elements - 1);
+    
+    
+    arr_ptr->head_ptr = new_head_ptr;
+    arr_ptr->n_elements = new_n_elements;
+    
 }
 
 bool get_by_idx(struct DynArrayInt* arr_ptr, int index, int* output){
@@ -41,7 +32,7 @@ bool get_by_idx(struct DynArrayInt* arr_ptr, int index, int* output){
         fprintf(stderr, "Index cannot be negative.\n");
         return false;
     } else if (index >= arr_ptr->n_elements) {
-        fprintf(stderr, "Index out of bound.\n");
+        fprintf(stderr, "Index out of bound: %d.\n", index);
         return false;
     } else {
         *output = *(arr_ptr->head_ptr + index);
@@ -49,13 +40,21 @@ bool get_by_idx(struct DynArrayInt* arr_ptr, int index, int* output){
     }
 }
 
+int get_length(struct DynArrayInt* arr_ptr){
+    return arr_ptr->n_elements;
+}
+
 int main(){
     
     struct DynArrayInt arr1;
     init_array(&arr1);
+    printf("Array has %d element(s)\n", get_length(&arr1));
     append(&arr1, 11);
+    printf("Array has %d element(s)\n", get_length(&arr1));
     append(&arr1, 12);
+    printf("Array has %d element(s)\n", get_length(&arr1));
     append(&arr1, 13);
+    printf("Array has %d element(s)\n", get_length(&arr1));
     
     // Getting values by index
     int output_0;
